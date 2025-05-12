2f5b0d68ecb20495ac66f1f9222803bad3724f6a
CUJeong <akow283@gmail.com>
2025-05-12 17:40:52 +0900

    end

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -2,21 +2,40 @@ import "./Post.css";
 import PostList from "./PostList";
 import PostDetail from "./PostDetail";
 import PostWrite from "./PostWrite";
+import PostEdit from "./PostEdit";
 import { Routes, Route } from "react-router-dom";
+// 2. 생성한 ContextPost 불러오기
+import { ContextPost } from "./postContext";
+import { useState } from "react";
+import { initPostArray } from "./postData";
 
 // Post 컴포넌트에서 PostList, PostDetail, PostWrite, PostEdit 화면전환
 function Post() {
+
+  // 하위 컴포넌트 여러곳에서 상위 컴포넌트인 Post의 postArray 변수를 사용하고자 함
+  // 일일히 props 로 넘겨주기보다는 context 를 이용하자!!
+  // 1. context 만들기 -> postContext.js 생성
+  const [postArray, setPostArray] = useState(initPostArray);
+
+
   return (
     <div>
       {/* Routes 로 post 관련 컴포넌트들 Route 등록 */}
       {/* Post 컴포넌트 진입시 주소 /post 이고, 화면은 PostList 컴포넌트*/}
-      <Routes>
-        {/* 현재 /는 /post와 같음 */}
-        <Route path="/" element={<PostList />} />
-        {/* 글쓰기 페이지 /write 는 /post/write 와 같음 */}
-        <Route path="/write" element={<PostWrite />} />
-        <Route path="/detail" element={<PostDetail />} />
-      </Routes>  
+      {/* 3. 해당 ContextPost를 사용할 하위 컴포넌트들을 감싸주기 */}
+      {/*    이후 공유할 데이터 넣어주기 (함수도 공유가능) */}
+      <ContextPost.Provider value={{postArray, setPostArray}}>
+        <Routes>
+          {/* 현재 /는 /post와 같음 */}
+          <Route path="/" element={<PostList />} />
+          {/* 글쓰기 페이지 /write 는 /post/write 와 같음 */}
+          <Route path="/write" element={<PostWrite />} />
+          {/* 상세보기시 /detail?no=23123 으로 요청 */}
+          <Route path="/detail" element={<PostDetail />} />
+          {/* 글수정시 /edit/23123 으로 요청 (path variable) */}
+          <Route path="/edit/:no" element={<PostEdit />} />
+        </Routes>  
+      </ContextPost.Provider>
     </div>
   );
 }
--- src/pages/Post/PostWrite.jsx
+++ src/pages/Post/PostWrite.jsx
@@ -1,7 +1,53 @@
-function PostWrite(){
-    return (
-        <div>글쓰기</div>
-    );
+import "./PostWrite.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ContextPost } from "./postContext";
+import { useContext } from "react";
+import { getDate } from "../../common/common.js"
+import { getNextNo } from "./postData.js";
+
+function PostWrite() {
+
+  const navigate = useNavigate();
+  const [title, setTitle] = useState();
+  const [content, setContent] = useState();
+
+  // context 에 담겨있는 postArray, setPostArray 모두 꺼냄
+  const {postArray, setPostArray} = useContext(ContextPost);
+
+  const writeDo = ()=>{
+    const temp = {};
+    temp.title = title;
+    temp.content = content;
+    temp.date = getDate();
+    temp.no = getNextNo(postArray);
+
+    postArray.push(temp);
+
+    // 라우터에 의해 페이지가 바뀌면 postArray가 변하지만
+    // 일반적으로 useState으로 관리중인 postArray에 대해 수정하는 경우
+    // setPostArray를 이용하는 것이 권장됨
+    setPostArray([...postArray]);
+
+    navigate("/post");
+  }
+
+  return (
+    <div className="write-container">
+      <input 
+        className="write-title" type="text" 
+        onChange={(e)=>setTitle(e.target.value)}
+        />
+      <textarea 
+        className="write-content"
+        onChange={(e)=>setContent(e.target.value)}
+        ></textarea>
+      <div className="text-align-end">
+        <button className="btn-empty" onClick={()=>navigate("/post")}>취소</button>
+        <button className="btn-fill" onClick={writeDo}>등록</button>
+      </div>
+    </div>
+  );
 }
 
 export default PostWrite;
